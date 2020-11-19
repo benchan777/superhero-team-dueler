029c2b4f50a12633ec70d9c3fbf453383db0ff9d
Ben Chan <benchan777@gmail.com>
2020-11-18 19:10:27 -0800

    added new dogs

--- my_dogs.py
+++ my_dogs.py
@@ -1,2 +1,16 @@
 # my_dogs.py
 import dog
+
+my_dog = dog.Dog("Rex", "SuperDog")
+my_dog.bark()
+
+my_other_dog = dog.Dog('Annie', 'SuperDog')
+print(my_other_dog.name)
+
+first_dog = dog.Dog('Alex', 'Golden Retriever')
+second_dog = dog.Dog('Sherlock', 'Shiba')
+third_dog = dog.Dog('Max', 'German Shepherd')
+
+first_dog.sit()
+second_dog.roll_over()
+third_dog.sit()
