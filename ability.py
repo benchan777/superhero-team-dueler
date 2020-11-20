57b6268d4cc4fe195fdab1c21a684501d97761c4
Ben Chan <benchan777@gmail.com>
2020-11-20 11:34:51 -0800

    add debugging ability

--- ability.py
+++ ability.py
@@ -14,3 +14,8 @@ class Ability:
         # Pick a random value between 0 and self.max_damage
         random_value = random.randint(0, self.max_damage)
         return random_value
+
+if __name__ == '__main__':
+    ability = Ability('Debugging Ability', 20)
+    print(ability.name)
+    print(ability.attack())
