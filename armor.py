9f1179504b43eb991258386be1becd7ba58876ab
Ben Chan <benchan777@gmail.com>
2020-11-20 11:34:34 -0800

    add armor class

---
+++ armor.py
@@ -0,0 +1,9 @@
+class Armor:
+    def __init__(self, name, max_block):
+        '''
+        Instantiate instance properties.
+        name: String
+        max_block: Integer
+        '''
+        self.name = name
+        self.max_block = max_block
