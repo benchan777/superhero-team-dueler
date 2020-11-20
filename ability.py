bbdb4260c518f089cca3c7cc97c56caa6cfb979d
Ben Chan <benchan777@gmail.com>
2020-11-20 11:21:20 -0800

    initial commit

---
+++ ability.py
@@ -0,0 +1,8 @@
+class Ability:
+    def __init__(self, name, max_damage):
+        '''
+        Initialize the values passed into this 
+        method as instance variables.
+        '''
+        self.name = name
+        self.max_damage = max_damage
