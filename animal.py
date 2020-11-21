b73fecc2200ece8c6e3be3f0e92c0daa5976711e
Ben Chan <benchan777@gmail.com>
2020-11-20 20:07:02 -0800

    add animal and frog class

---
+++ animal.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f"{self.name} is eating.")
+
+    def drink(self):
+        print(f"{self.name} is drinking.")
+
+class Frog(Animal):
+    def jump(self):
+        print(f"{self.name} is jumping.")
+
+my_dog = Animal("Sophie")
+my_dog.eat()
+my_dog.drink()
+my_dog.jump()
+
+frog = Frog("Tim")
+frog.jump()
+frog.drink()
