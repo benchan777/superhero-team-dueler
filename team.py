f3e8f1b2d1d0b21a12b366d850de9e59d5700b48
Ben Chan <benchan777@gmail.com>
2020-11-21 01:40:07 -0800

    initial commit

---
+++ team.py
@@ -0,0 +1,7 @@
+class Team:
+    def __init__(self, name):
+        ''' Initialize your team with its team name and
+            an empty list of heroes
+        '''
+        self.name = name
+        self.heroes = list()
