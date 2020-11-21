055ae86db7ebca948a34d396f6d0c273dc780f2b
Ben Chan <benchan777@gmail.com>
2020-11-21 02:41:32 -0800

    add revive_heroes and attack methods

--- team.py
+++ team.py
@@ -1,3 +1,6 @@
+import random
+from hero import Hero
+
 class Team:
     def __init__(self, name):
         ''' Initialize your team with its team name and
@@ -36,3 +39,29 @@ class Team:
                 hero.deaths += 1
             kd = hero.kills / hero.deaths
             print("{} Kill/Deaths:{}".format(hero.name,kd))
+
+    def revive_heroes(self, health=100):
+        ''' Reset all heroes health to starting_health '''
+        for hero in self.heroes:
+            hero.current_health(health)
+
+    def attack(self, other_team):
+        ''' Battle each team against each other. '''
+        living_heroes = list()
+        living_opponents = list()
+
+        for hero in self.heroes:
+            living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+        
+        while len(living_heroes) > 0 and len(living_opponents) > 0:
+            random_hero = random.choice(living_heroes)
+            random_opponent = random.choice(living_opponents)
+            random_hero.fight(random_opponent)
+
+            if random_hero.current_health > random_opponent.current_health:
+                living_opponents.remove(random_opponent)
+            else:
+                living_heroes.remove(random_hero)
